52a2da9fbb3d245ebe4e7bf284bb26b19c1d1030
John Harison <john.harison99@gmail.com>
2019-02-12 15:43:48 +0700

    salah ketik

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/',function(req,res) => res.send("Hello World"));
+app.get('/',(req,res) => res.send("Hello World"));
 
-app.listen(8080,function(req,res) => console.log("Hello World"));
+app.listen(8080,(req,res) => console.log("Hello World"));
